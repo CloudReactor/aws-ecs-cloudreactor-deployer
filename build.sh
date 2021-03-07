7df2ad6385f7b2f7917b27a149165a998875761d
jtsay <jtsay@cloudreactor.io>
2021-03-06 23:35:20 -0800

    Comments

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# This script builds the deployer image, and is not needed unless
+# you're working on the aws-ecs-cloudreactor-deployer project.
+
 set -e
 
-docker build -t aws-ecs-cloudreactor-deployer -t cloudreactor/aws-ecs-cloudreactor-deployer .
+docker build -t aws-ecs-cloudreactor-deployer -t \
+ cloudreactor/aws-ecs-cloudreactor-deployer .
