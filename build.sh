ccaef61c55d178c061b936384a705f8eeb79aa11
jtsay <jtsay@cloudreactor.io>
2021-05-19 00:04:52 -0700

    Release 1.2.1

--- build.sh
+++ build.sh
@@ -7,12 +7,13 @@ set -e
 docker build -t aws-ecs-cloudreactor-deployer -t \
  cloudreactor/aws-ecs-cloudreactor-deployer .
 
-docker tag aws-ecs-cloudreactor-deployer cloudreactor/aws-ecs-cloudreactor-deployer:1.2.0
+docker tag aws-ecs-cloudreactor-deployer cloudreactor/aws-ecs-cloudreactor-deployer:LATEST
+docker tag aws-ecs-cloudreactor-deployer cloudreactor/aws-ecs-cloudreactor-deployer:1.2.1
 docker tag aws-ecs-cloudreactor-deployer cloudreactor/aws-ecs-cloudreactor-deployer:1.2
 docker tag aws-ecs-cloudreactor-deployer cloudreactor/aws-ecs-cloudreactor-deployer:1
 
-# docker login
-#
-# docker push cloudreactor/aws-ecs-cloudreactor-deployer:1.2.0
-# docker push cloudreactor/aws-ecs-cloudreactor-deployer:1.2
-# docker push cloudreactor/aws-ecs-cloudreactor-deployer:1
+docker login
+docker push cloudreactor/aws-ecs-cloudreactor-deployer:LATEST
+docker push cloudreactor/aws-ecs-cloudreactor-deployer:1.2.1
+docker push cloudreactor/aws-ecs-cloudreactor-deployer:1.2
+docker push cloudreactor/aws-ecs-cloudreactor-deployer:1
