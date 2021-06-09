88385edeaa29a110a5de9310edfa79a04a131288
jtsay <jtsay@cloudreactor.io>
2021-06-08 18:51:00 -0700

    Release 1.2.4

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script releases the deployer image, and is not needed unless
+# you're a maintainer of the aws-ecs-cloudreactor-deployer project.
+
+set -e
+
+docker tag aws-ecs-cloudreactor-deployer cloudreactor/aws-ecs-cloudreactor-deployer:1.2.4
+docker tag aws-ecs-cloudreactor-deployer cloudreactor/aws-ecs-cloudreactor-deployer:1.2
+docker tag aws-ecs-cloudreactor-deployer cloudreactor/aws-ecs-cloudreactor-deployer:1
+
+docker login
+docker push cloudreactor/aws-ecs-cloudreactor-deployer:latest
+docker push cloudreactor/aws-ecs-cloudreactor-deployer:1.2.4
+docker push cloudreactor/aws-ecs-cloudreactor-deployer:1.2
+docker push cloudreactor/aws-ecs-cloudreactor-deployer:1
